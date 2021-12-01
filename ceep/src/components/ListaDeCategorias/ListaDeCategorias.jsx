7b6286b283c9e77bf13e99771a26e896a84bcebd
elton-souza <elton.souza1515@gmail.com>
2021-12-01 17:52:15 -0300

    Um novo componente que adiciona categorias e exibe elas

---
+++ ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import "./style.css"
+class ListaDeCategorias extends Component {
+    _handleEventoInput(e){
+        let inputValue = e.target.value
+        if(e.key === "Enter"){
+           this.props.adicionarCategoria(inputValue)
+           e.target.value = ""
+        }
+    }
+    render() { 
+        return (
+            <section className="lista-categorias">
+                <ul className="lista-categorias_lista">
+                    {this.props.categorias.map((categoria,index)=>{
+                       return <li key={index} className="lista-categorias_item">{categoria}</li>   
+                    })}
+                </ul>
+                <input type="text" placeholder="Adicionar Categoria" onKeyUp={this._handleEventoInput.bind(this)}/>
+            </section>
+        );
+    }
+}
+ 
+export default ListaDeCategorias;
